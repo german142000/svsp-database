647295f7da9863cc1d151598f935146557f321a1
svsp <77344156+german142000@users.noreply.github.com>
2022-03-28 12:45:04 +0300

    Create main.php

---
+++ svsp-database/main.php
@@ -0,0 +1,36 @@
+<!-- Author - Fonteyn German - svsp -->
+<?php
+if($_GET['r'] == 'parol' && $_GET['user'] == 'admin'){
+$dir = $_SERVER['DOCUMENT_ROOT']."/svsp-database/".$_GET['tableName']."/".$_GET['column'].".txt";
+  
+if($_GET['f'] == 'getCell'){
+$file = file($dir, FILE_IGNORE_NEW_LINES);
+$cell = $file[$_GET['num']];
+echo $cell;
+}
+  
+if($_GET['f'] == 'repCell'){
+$file = file($dir, FILE_IGNORE_NEW_LINES);
+  
+if($file[$_GET['num']] != null){
+$file[$_GET['num']] = $_GET['content'];
+file_put_contents($dir, implode(PHP_EOL, $file));
+echo 'true';
+} 
+else echo 'false';
+  
+}
+
+if($_GET['f'] == 'setCell'){
+$file = file($dir, FILE_IGNORE_NEW_LINES);
+$file[] = $_GET['content'];
+file_put_contents($dir, implode(PHP_EOL, $file));
+}
+
+if($_GET['f'] == 'getArray'){
+$file = file($dir, FILE_IGNORE_NEW_LINES);
+echo $file;
+}
+  
+}
+?>
